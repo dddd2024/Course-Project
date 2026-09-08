c780ec7e6ed0e7b0fe9a37130ce31923e3067f1f
dddd2024 <2777689227@qq.com>
2026-09-08 22:28:25 +0800

    feat(track-a): export provisional parser coverage API

--- src/course_project/verification/__init__.py
+++ src/course_project/verification/__init__.py
@@ -6,9 +6,23 @@ from course_project.verification.checks import (
     verify_length,
     verify_sequence,
 )
+from course_project.verification.provisional_parser import (
+    CorpusKind,
+    ParsedField,
+    ParseCoverageReport,
+    ParseSample,
+    ParseSampleResult,
+    execute_provisional_schema,
+)
 
 __all__ = [
+    "CorpusKind",
+    "ParsedField",
+    "ParseCoverageReport",
+    "ParseSample",
+    "ParseSampleResult",
     "VerificationError",
+    "execute_provisional_schema",
     "verification_result",
     "verify_length",
     "verify_sequence",
